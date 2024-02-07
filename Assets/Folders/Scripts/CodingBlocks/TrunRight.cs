50632124bf950f70535e0e365c298aec96d07b96
JIN9830 <101498377+JIN9830@users.noreply.github.com>
2024-02-07 23:56:42 +0900

    save but TODO: fix the rotation bug
    DoRotateQuaternion need to fix

--- Assets/Folders/Scripts/CodingBlocks/TrunRight.cs
+++ Assets/Folders/Scripts/CodingBlocks/TrunRight.cs
@@ -1,12 +1,18 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TrunRight : CodingBlock
 {
+    Quaternion playerStartRot;
+    Quaternion playerEndRot;
     public override void MoveOrder()
     {
+        playerStartRot = Gm.playerObject.transform.rotation;
+        playerEndRot = playerStartRot * Quaternion.Euler(0, 90, 0);
         ToggleHighLight(true);
-        Gm.playerObject.transform.rotation *= Quaternion.Euler(0, 90, 0);
+        Gm.playerObject.transform.DORotateQuaternion(playerEndRot, 0.3f);
+        Gm.playerAnimator.SetTrigger("TurnRight");
     }
 }
--- Assets/Folders/Scripts/CodingBlocks/TurnLeft.cs
+++ Assets/Folders/Scripts/CodingBlocks/TurnLeft.cs
@@ -1,12 +1,19 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TurnLeft : CodingBlock
 {
+    Quaternion playerStartRot;
+    Quaternion playerEndRot;
+
     public override void MoveOrder()
     {
+        playerStartRot = Gm.playerObject.transform.rotation;
+        playerEndRot = playerStartRot * Quaternion.Euler(0,-90,0);
         ToggleHighLight(true);
-        Gm.playerObject.transform.rotation *= Quaternion.Euler(0, -90, 0);
+        Gm.playerObject.transform.DORotateQuaternion(playerEndRot, 0.3f);
+        Gm.playerAnimator.SetTrigger("TurnLeft");
     }
 }
