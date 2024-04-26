4a0ac27a48876ce4a945cb58a7c8ea3c17a70194
JIN9830 <101498377+JIN9830@users.noreply.github.com>
2024-04-26 23:32:44 +0900

    Coin

--- Assets/Folders/Scripts/StageManager/Coin.cs
+++ Assets/Folders/Scripts/StageManager/Coin.cs
@@ -3,19 +3,21 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    private void OnEnable()
+    {
+        gameObject.transform.localScale = new Vector3(0, 0, 0);
+        gameObject.transform.DOScale(new Vector3(1.2f, 1.2f, 1.2f), 1.5f).SetEase(Ease.OutBounce);
+    }
 
     private void Start()
     {
-        
+        gameObject.transform.DORotate(new Vector3(0, -360, 0), 1.5f, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Incremental).SetEase(Ease.Linear);
     }
 
     public void OnTriggerEnter(Collider other)
     {
-        Debug.Log("콜라이더 충돌!");
         if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("콜라이더 조건 달성!");
-
             GameManager.StageManager_Instance.UpdateCoin();
             this.gameObject.SetActive(false);
         }
