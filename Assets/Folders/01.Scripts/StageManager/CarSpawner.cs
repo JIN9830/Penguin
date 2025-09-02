1c339aa9f4af39119d40d206e789159d60900141
JIN9830 <101498377+JIN9830@users.noreply.github.com>
2025-09-02 23:19:22 +0900

    변수명 수정 ( _ <- 제거 )

--- Assets/Folders/01.Scripts/StageManager/CarSpawner.cs
+++ Assets/Folders/01.Scripts/StageManager/CarSpawner.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class CarSpawner : MonoBehaviour
 {
     [SerializeField]
-    private List<CarController> _cars = new List<CarController>();
+    private List<CarController> cars = new List<CarController>();
 
     [SerializeField]
-    private List<GameObject> _roads = new List<GameObject>();
-    private List<GameObject> _roadStartPosition = new List<GameObject>();
+    private List<GameObject> roads = new List<GameObject>();
+    private List<GameObject> roadStartPosition = new List<GameObject>();
 
     [SerializeField]
-    private int _carSpawnDelay = 5;
+    private int carSpawnDelay = 5;
     [SerializeField]
-    private int _carMovingDistance = 10;
+    private int carMovingDistance = 10;
     [SerializeField]
-    private float _carMovingSpeed = 8;
+    private float carMovingSpeed = 8;
 
     private void Awake()
     {
@@ -28,29 +28,29 @@ public class CarSpawner : MonoBehaviour
             var car = transform.GetChild(i).GetComponent<CarController>();
             if (car != null)
             {
-                _cars.Add(car);
+                cars.Add(car);
                 car.gameObject.SetActive(false);
             }
         }
 
         // .. 도로 오브젝트를 순회하며 도로 시작 지점 오브젝트를 리스트에 추가
-        int roadCount = _roads.Count;
+        int roadCount = roads.Count;
 
         for (int i = 0; i < roadCount; i++)
         {
-            GameObject roadStartPoint = _roads[i].gameObject.transform.GetChild(0).gameObject;
-            _roadStartPosition.Add(roadStartPoint);
+            GameObject roadStartPoint = roads[i].gameObject.transform.GetChild(0).gameObject;
+            roadStartPosition.Add(roadStartPoint);
         }
     }
 
     private void Start()
     {
-        if (_cars.Count > 0) StartCoroutine(SpawnCar());
+        if (cars.Count > 0) StartCoroutine(SpawnCar());
     }
 
     private IEnumerator SpawnCar()
     {
-        var waitForCarSpawnTime = new WaitForSeconds(_carSpawnDelay);
+        var waitForCarSpawnTime = new WaitForSeconds(carSpawnDelay);
 
         while (true)
         {
@@ -59,8 +59,8 @@ public class CarSpawner : MonoBehaviour
             if (availableCar != null)
             {
                 availableCar.gameObject.SetActive(true);
-                Transform startPos = _roadStartPosition[Random.Range(0, _roadStartPosition.Count)].transform;
-                availableCar.MoveCar(_carMovingDistance, _carMovingSpeed, startPos);
+                Transform startPos = roadStartPosition[Random.Range(0, roadStartPosition.Count)].transform;
+                availableCar.MoveCar(carMovingDistance, carMovingSpeed, startPos);
             }
 
             yield return waitForCarSpawnTime;
@@ -69,11 +69,11 @@ public class CarSpawner : MonoBehaviour
 
     private CarController FindAvailableCar()
     {
-        for (int i = 0; i < _cars.Count; i++)
+        for (int i = 0; i < cars.Count; i++)
         {
-            if (!_cars[i].isMoving)
+            if (!cars[i].isMoving)
             {
-                return _cars[i];
+                return cars[i];
             }
         }
         return null;
